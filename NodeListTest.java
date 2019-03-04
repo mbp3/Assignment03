f8edf56037dff10c8a8036d20c1acfe4fa136fbe
mbp3 <mbp3@pct.edu>
2019-03-04 12:24:47 -0500

    code back to original test status - can work on tests

--- NodeListTest.java
+++ NodeListTest.java
@@ -17,8 +17,6 @@ import org.junit.jupiter.api.Test;
 class NodeListTest {
 
 	static NodeList testList = new NodeList();
-	static NodeList testList2 = new NodeList();
-	static NodeList testList3 = new NodeList();
 	static ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 	static PrintStream oldOutStream = System.out;
 	
@@ -31,20 +29,11 @@ class NodeListTest {
 	
 	@Before
 	void initTestList() {
-		testList.insert(0, new NodeList.Node("Bravo"));
-		testList.insert(1, new NodeList.Node("Delta"));
-		testList.insert(2, new NodeList.Node("Echo"));
-		testList.insert(3, new NodeList.Node("Foxtrot"));
-		
-		testList2.insert(0, new NodeList.Node("Bravo"));
-		testList2.insert(1, new NodeList.Node("Delta"));
-		testList2.insert(2, new NodeList.Node("Echo"));
-		testList2.insert(3, new NodeList.Node("Foxtrot"));
-		
-		testList3.insert(0, new NodeList.Node("Bravo"));
-		testList3.insert(1, new NodeList.Node("Delta"));
-		testList3.insert(2, new NodeList.Node("Echo"));
-		testList3.insert(3, new NodeList.Node("Foxtrot"));
+		testList.addHead(new NodeList.Node("Bravo"));
+		//testList.insert(0, new NodeList.Node("Bravo"));
+		//testList.insert(1, new NodeList.Node("Delta"));
+		//testList.insert(2, new NodeList.Node("Echo"));
+		//testList.insert(3, new NodeList.Node("Foxtrot"));
 	}
 	
 	@After
@@ -52,16 +41,14 @@ class NodeListTest {
 		System.setOut(oldOutStream);
 	}
 	
-//	@AfterEach
-//	void destroyTestList() {
-//		int length = testList.getLength();
-//		for(int i = 0; i < length; i++)
-//		{
-//			testList.remove(i);
-//		}
-//			
-//		testList.setLength(0);
-//	}
+	@AfterEach
+	void destroyTestList() {
+		int length = testList.getLength();
+		for(int i = 0; i < length; i++)
+		{
+			testList.remove(i);
+		}
+	}
 	
 	/****** TEST CASES ******/
 	
@@ -92,19 +79,19 @@ class NodeListTest {
 	void addItemEndTest() {
 		// Should have 4 Items from initTestList()
 		assertEquals(4,
-					 testList2.getLength(),
+					 testList.getLength(),
 					 "Add End Test starting condtions not met.");
 		
 		testList.addTail(new NodeList.Node("Golf"));
 		
 		assertEquals(5,
-					 testList2.getLength(),
+					 testList.getLength(),
 					 "Add End Test: addTail() does not increase list length.");
 		
-		assertTrue(testList2.getTail().getName().equals("Golf"),
+		assertTrue(testList.getTail().getName().equals("Golf"),
 				   "Add End Test: addTail() does not insert Golf Node.");
 		
-		testList2.print();
+		testList.print();
 		
 		assertEquals("Bravo\nDelta\nEcho\nFoxtrot\nGolf",
 					 outStream.toString(),
@@ -115,19 +102,19 @@ class NodeListTest {
 	void insertItemAtPositionTest() {
 		// Should have 4 Items from initTestList()
 		assertEquals(4,
-					 testList3.getLength(),
+					 testList.getLength(),
 					 "Insert Test starting conditions not met.");
 		
-		testList3.insert(0, new NodeList.Node("Alpha"));
+		testList.insert(0, new NodeList.Node("Alpha"));
 		
-		testList3.insert(2, new NodeList.Node("Charlie"));
+		testList.insert(2, new NodeList.Node("Charlie"));
 		
 		assertAll("list inserts",
-				() -> assertEquals(6, testList3.getLength()),
-				() -> assertEquals("Charlie", testList3.get(2).getName())
+				() -> assertEquals(6, testList.getLength()),
+				() -> assertEquals("Charlie", testList.get(2).getName())
 			);
 		
-		testList3.print();
+		testList.print();
 		
 		assertEquals("Alpha\nBravo\nCharlie\nDelta\nEcho\nFoxtrot",
 					 outStream.toString(),
